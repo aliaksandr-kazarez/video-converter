20e15042895a92190394e0e152e5cf20f8c63bf9
Alex Kazarez <akazarez@amazon.com>
2024-12-06 16:40:26 -0800

    Initial Commit

---
+++ VideoConverter/VideoConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoConverterApp.swift
+//  VideoConverter
+//
+//  Created by Kazarez, Alex on 12/6/24.
+//
+
+import SwiftUI
+
+@main
+struct VideoConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
